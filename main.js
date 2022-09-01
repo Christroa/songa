a7e3718497941db595529b8e8776e803b55f575c
Collins Ikechukwu <collinschristroa@gmail.com>
2022-09-01 15:45:42 +0100

    Partially done with Homepage Desktop version 0.2

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const sidebar = document.querySelector(".sidebar");
+const heroSlide = document.querySelector(".heroSlide");
 const sidemenuToggle = document.querySelector(".sidemenutoggle");
 const sidemenuText = document.querySelectorAll(".sidemenuText");
 sidemenuToggle.addEventListener("click", () => {
@@ -14,4 +15,107 @@ sidemenuToggle.addEventListener("click", () => {
       sidebar.style = "flex: ''; min-width: 0; max-width: 80px";
     }
   });
+});
+
+setInterval(() => {
+  let bodyWidth = document.body.clientWidth;
+  const checkBodyWidth = (() => {
+    sidemenuText.forEach((text) => {
+      if (bodyWidth <= 768) {
+        text.style.display = "none";
+        text.parentElement.style = "justify-content: center";
+        sidebar.style = "flex: ''; min-width: 0; max-width: 80px";
+        // heroSlide.style = "height: 40px";
+      } else {
+        text.style.display = "inline";
+        text.parentElement.style = "justify-content: start";
+        sidebar.style = "flex: 1; min-width: 240; max-width: 250";
+      }
+    });
+  })();
+}, 500);
+
+///Swiper Settings😎
+const swiper = new Swiper(".heroslide", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: true,
+  loop: true,
+});
+
+////Top selling slide Config
+const swiper1 = new Swiper(".topselling", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  autoplay: true,
+});
+
+////Flash Sale SLide COnfig
+const swiper3 = new Swiper(".flashSale", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  autoplay: true,
 });
